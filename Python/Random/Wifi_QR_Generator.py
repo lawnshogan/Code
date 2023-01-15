a45617223d63ca2dd3ecab27a623eee54be93f78
LawnShogan <shawnlogan.gis@gmail.com>
2023-01-14 20:35:44 -0700

    QR code generator for home wifi

--- Python/Random/Wifi_QR_Generator.py
+++ Python/Random/Wifi_QR_Generator.py
@@ -1,2 +1,12 @@
-import wifi_qrcode_generator as qr
-qr.wifi_gen('wifi name ', False, 'WPA', 'password')
+import qrcode
+
+def generate_qr_code(ssid, password):
+    wifi_qr = qrcode.QRCode(version=1, box_size=10, border=5)
+    wifi_qr.add_data(f'WIFI:S:{ssid};T:WPA;P:{password};;')
+    wifi_qr.make(fit=True)
+    img = wifi_qr.make_image(fill_color='black', back_color='white')
+    img.save(f'{ssid}.png')
+    print(f'QR code saved as {ssid}.png')
+
+# Example usage
+generate_qr_code('Trojan Virus Downloader', '9250W73rdPlace')
