f8f048d2a78f987ba25c1268214c985fb3ce31a4
lawnshogan <shawnlogan.gis@gmail.com>
2023-01-25 08:46:47 -0700

    Web Scraping EPA script

---
+++ Python/GIS/Export_Selected_Records.py
@@ -0,0 +1,19 @@
+import arcpy
+
+# Set the workspace to the GDB location
+arcpy.env.workspace = r"G:\Shared drives\SLB Business Intelligence\Data Services\GIS\Projects\Active\2023\SLB Wall Maps\2023\WallMapData.gdb"
+
+# Set the input feature class
+in_fc = "USA_Federal_Lands"
+
+# Create a list of unique values in the "Agency" field
+unique_values = list(set([row[0] for row in arcpy.da.SearchCursor(in_fc, ["Agency"])]))
+
+# Loop through the unique values
+for value in unique_values:
+    # use the value from the "Agency" field as the output feature class name
+    out_fc = value
+    # Create a where clause to select the appropriate features
+    where_clause = '"Agency" = ' + "'" + value + "'"
+    # Use the Select_analysis tool to create a new feature class with the selected features
+    arcpy.Select_analysis(in_fc, out_fc, where_clause)
