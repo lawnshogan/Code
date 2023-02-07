c48873ef399b05ce6dcf2de9468c0e86980e82ff
lawnshogan <shawnlogan.gis@gmail.com>
2023-02-07 12:26:59 -0700

    Redo Loop

--- Python/GIS/Export_Selected_Records.py
+++ Python/GIS/Export_Selected_Records.py
@@ -9,11 +9,4 @@ in_fc = "USA_Federal_Lands"
 # Create a list of unique values in the "Agency" field
 unique_values = list(set([row[0] for row in arcpy.da.SearchCursor(in_fc, ["Agency"])]))
 
-# Loop through the unique values
-for value in unique_values:
-    # use the value from the "Agency" field as the output feature class name
-    out_fc = value
-    # Create a where clause to select the appropriate features
-    where_clause = '"Agency" = ' + "'" + value + "'"
-    # Use the Select_analysis tool to create a new feature class with the selected features
-    arcpy.Select_analysis(in_fc, out_fc, where_clause)
+
