9ef170efb88ebbe04a745feed36d1951ac22b72f
lawnshogan <shawnlogan.gis@gmail.com>
2023-06-01 07:56:04 -0600

    Add if statment to check if relatedAssets is an object - not string

--- JS/Netsuite/related_assets_user_event.js
+++ JS/Netsuite/related_assets_user_event.js
@@ -11,10 +11,12 @@ define([], function() {
         var rec = context.newRecord;
         var relatedAssets = rec.getValue('custrecord_hein_inspection_related_asset');
 
-        // Check if relatedAssets is a string before trying to use replace
-        if (typeof relatedAssets === 'string' || relatedAssets instanceof String) {
-            var formattedRelatedAssets = relatedAssets.replace(/ FAM/g, '\nFAM');
-            rec.setValue('custrecord_hein_inspection_related_asset', formattedRelatedAssets);
+        if (Array.isArray(relatedAssets)) {
+            var formattedRelatedAssets = relatedAssets.map(function(asset) {
+                return asset.text.replace(/ FAM/g, '\nFAM');
+            });
+
+            rec.setValue('custrecord_hein_inspection_related_asset', formattedRelatedAssets.join('\n'));
         }
     }
 
